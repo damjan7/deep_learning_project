fcc21bd7638cfd27d7b92aa5130e29feea9ff828
abibaj98 <101990020+abibaj98@users.noreply.github.com>
2023-01-11 23:40:32 +0100

    Add files via upload

--- Overview.py
+++ Overview.py
@@ -5,7 +5,7 @@
 " Networks "            # 2 Networks:           -FCNN   -CNN
 " Priors "              # 5 Priors:             -Gausiaan   -StudentT   -Slab&Spike  -NIG    -Pretrained
 " Temperatures "        # 5 Temperatures:       -1e-3   -5e-3    -1e-2    -5e-2    -1e-1    -5e-1    -1 
-" Sample Size"          # 3 Sample Sizes:-10'000    -60'000     -120'000
+" Sample Size"          # 3 Sample Sizes:       -10'000    -60'000     -120'000
 
 
 """"""""""""""""""""""""""""""""
