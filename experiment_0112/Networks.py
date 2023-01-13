530655422aeb915d1f8bacfa4eafb35031a1b4fe
Karim305 <70638388+Karim305@users.noreply.github.com>
2023-01-13 11:38:26 +0100

    added code to run on cluster

---
+++ experiment_0112/Networks.py
@@ -0,0 +1,69 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+""" Base Networks """
+
+""" Fully Connected Neural Network """
+
+""" - 3 Layers (2 Hidden, 1 Output)
+    - 100 units each for hidden layers
+    - ReLu activations
+    """
+
+class FullyConnectedNN(nn.Module):
+    def __init__(self, in_features = 28*28, out_features = 10, hidden_units = 100, hidden_layers = 2):
+        super(FullyConnectedNN, self).__init__()
+
+        # Create layer list
+        self.layers = []
+
+        # Input to first layer
+        self.layers.append(nn.Linear(in_features, hidden_units))
+        self.layers.append(nn.ReLU())
+
+        # Hidden layers
+        for i in range(hidden_layers - 1):
+            self.layers.append(nn.Linear(hidden_units, hidden_units))
+            self.layers.append(nn.ReLU())
+        
+        # Output layer
+        self.layers.append(nn.Linear(hidden_units, out_features))
+
+        # Convert to sequential
+        self.layers = nn.Sequential(*self.layers)
+
+    def forward(self, x):
+        x = x.reshape(-1,28*28)
+        output = self.layers(x)
+        return output
+
+
+""" Convolutional Neural Network """
+
+""" - 3 Layers (2 Hidden, 1 Output)
+    - First two layers: Convolutional Layers with 64 channels, 3x3 convolutions, followed by 2x2 MaxPooling
+    - All layers use ReLu activations
+    """
+
+class ConvolutionalNN(nn.Module):
+    def __init__(self):
+        super(ConvolutionalNN, self).__init__()
+        
+        # Create Network
+        self.layers = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=8, kernel_size=3, padding=1),
+            nn.ReLU(inplace = True),
+            nn.MaxPool2d(kernel_size=2),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
+            nn.ReLU(inplace = True),
+            nn.MaxPool2d(kernel_size=2),
+            nn.Flatten(),
+            nn.Linear(64 * 7 * 7, 10))
+        
+    def forward(self, x):
+        x = x.view(-1, 1, 28, 28)
+        output = self.layers(x)
+        return output
---
+++ experiment_0112/run_experiment.py
@@ -0,0 +1,132 @@
+"""
+This script runs the experiment for our project.
+
+"""
+
+# Importing libraries ---------------------------------------------------------
+
+import numpy as np
+import torch
+import torchvision.transforms as tr
+import pandas as pd
+
+from data import Data
+from priors import *
+from Networks import *
+from BayesianNN import BNN_MCMC
+
+# Setting seeds ---------------------------------------------------------------
+torch.manual_seed(0)
+
+
+# Specify the prior -----------------------------------------------------------
+
+# Possible prior choices: 
+#       Isotropic_Gaussian, 
+#       StudentT_prior
+#       Laplace_prior
+#       Gaussian_Mixture
+#       Normal_Inverse_Gamma
+#       GaussianSpikeNSlab
+#       MixedLaplaceUniform
+
+device = "cuda"
+
+
+# prior = Isotropic_Gaussian()
+# prior = StudentT_prior()
+# prior = Gaussian_Mixture()
+# prior = Normal_Inverse_Gamma(device = device)
+prior = GaussianSpikeNSlab(device = device)
+# prior = MixedLaplaceUniform()
+
+# Specify the iteration parameters --------------------------------------------
+
+# network list
+networks = {"FCNN": FullyConnectedNN(),
+          "CNN": ConvolutionalNN()}
+
+# Temperature list
+Temperatures = [0.001, 0.01, 0.1, 1, 10]
+
+# sample size list
+sample_sizes = [3750, 15000, 60000, 120000]
+
+# preallocate pandas dataframe for results
+results = pd.DataFrame(columns = [
+    "Network", 
+    "Sample Size", 
+    "Epochs", 
+    "Burn in", 
+    "sample interval", 
+    "Temperature", 
+    "Test Accuracy", 
+    "Test ECE", 
+    "Test AUROC"], 
+    index = range(len(networks)*len(Temperatures)*len(sample_sizes)))
+
+
+#create a dict for the different parameter values
+base_epoch, base_burn_in, base_sample_interval, base_samplesize = 50, 10, 2, sample_sizes[-1]
+args_dict = [(sample_size, (base_epoch*base_samplesize/sample_size, base_burn_in*base_samplesize/sample_size, base_sample_interval*base_samplesize/sample_size )) for sample_size in sample_sizes]
+args_dict = dict(args_dict)
+
+# Run the experiment ----------------------------------------------------------
+
+iteration = 0
+
+for net in networks.keys():
+    for T in Temperatures:
+        for n in range(len(sample_sizes)):
+        
+            """ 
+            # print iteration info
+            print(50*"-")
+            print("Iteration: ", iteration, " of ", len(networks)*len(Temperatures)*len(sample_sizes))
+            print("Network:     ", net)
+            print("Prior:       ", prior.name)
+            print("Temperature: ", T)
+            print("Sample size: ", sample_sizes[n])
+            print("Epoch:       ", args_dict[sample_sizes[n]][0])
+            print("Burn in:     ", args_dict[sample_sizes[n]][1])
+            print("Sample interval: ", args_dict[sample_sizes[n]][2])
+            """
+
+            # get data
+            if sample_sizes[n] == 120000:
+                # if sample size is 120000, use data augmentation
+                augmentations = tr.Compose([tr.RandomRotation(15)])
+                train_data, test_data = Data("MNIST", augmentations = augmentations).get_data()
+            else:
+                # subsample original train data if sample size is smaller than 120000
+                train_data, test_data = Data("MNIST", augmentations = None).get_data(num_train_samples=sample_sizes[n])
+
+
+            # run BNN
+            model = BNN_MCMC(
+                train_data,
+                network = networks[net],
+                prior=prior,
+                Temperature = T,
+                num_epochs = int(args_dict[sample_sizes[n]][0]),
+                max_size = 10,
+                burn_in = int(args_dict[sample_sizes[n]][1]),
+                lr = 1e-3,
+                sample_interval = int(args_dict[sample_sizes[n]][2]),
+                device=device)
+
+            model.train()
+
+            # get test metrics
+            acc, ece, auroc = model.get_metrics(test_data)
+
+            #print("Test accuracy: ", acc)
+            #print("Test ECE: ", ece)
+            #print("Test AUROC: ", auroc)
+
+            # save results
+            results.iloc[iteration, :] = net, sample_sizes[n], args_dict[sample_sizes[n]][0], args_dict[sample_sizes[n]][1], args_dict[sample_sizes[n]][2], T, acc, ece, auroc
+            iteration += 1
+
+# save results to csv
+results.to_csv(f"results/results_{prior.name}.csv")
